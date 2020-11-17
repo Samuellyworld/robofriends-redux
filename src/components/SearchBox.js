e58fda8d74119e5af10471f2ebc22d5d7701a88b
samuel <samuellyworld@gmail.com>
2020-11-16 16:23:17 -0800

    serviceWorker

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
       <input
+        arial-label="search robots"
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {createLogger} from 'redux-logger';
 import {createStore, applyMiddleware, combineReducers} from 'redux';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import {searchRobots, requestRobots} from './reducer.js';
 import 'tachyons';
 
@@ -17,4 +17,7 @@ ReactDOM.render(
              <Provider store= {store} >
 	          <App />
 	         </Provider>, document.getElementById('root'));
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
